a47ba27d25a8fc0a76601470b8fe30c389fea2e2
Nawaz-swivel <nawas@swivelgroup.com.au>
2023-04-01 18:18:57 +0530

    added main application test

--- src/test/java/com/swivel/ignite/payment/PaymentApplicationTests.java
+++ src/test/java/com/swivel/ignite/payment/PaymentApplicationTests.java
@@ -1,6 +1,9 @@
 package com.swivel.ignite.payment;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * This class tests {@link PaymentApplication} class
@@ -11,10 +14,10 @@ class PaymentApplicationTests {
     /**
      * This method tests Spring application main run method
      */
-//	@Test
-//	void Should_RunSpringApplication() {
-//		PaymentApplication.main(new String[]{});
-//
-//		assertTrue(true, "Spring Application Context Loaded Successfully");
-//	}
+	@Test
+	void Should_RunSpringApplication() {
+		PaymentApplication.main(new String[]{});
+
+		assertTrue(true, "Spring Application Context Loaded Successfully");
+	}
 }
